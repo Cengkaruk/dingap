77b975a8bc420d576b7d782239601065f6c910ad
pbaldwin <pbaldwin@d196f2e3-cd94-45c7-a57a-680589916e42>
2010-10-15 00:33:28 +0000

    - Changed default language from english to en_US
    git-svn-id: svn://svn.clearfoundation.com/clearos@144 d196f2e3-cd94-45c7-a57a-680589916e42

--- framework/trunk/application/config/config.php
+++ framework/trunk/application/config/config.php
@@ -68,7 +68,7 @@ $config['url_suffix'] = "";
 | than english.
 |
 */
-$config['language']	= "english";
+$config['language']	= "en_US";
 
 /*
 |--------------------------------------------------------------------------
