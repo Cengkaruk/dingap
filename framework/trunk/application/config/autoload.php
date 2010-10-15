0c8da62b53e5e012860d6c442e896ba7abcb539b
pbaldwin <pbaldwin@d196f2e3-cd94-45c7-a57a-680589916e42>
2010-10-15 00:21:46 +0000

    - Added webconfig and language to autoload
    git-svn-id: svn://svn.clearfoundation.com/clearos@137 d196f2e3-cd94-45c7-a57a-680589916e42

--- framework/trunk/application/config/autoload.php
+++ framework/trunk/application/config/autoload.php
@@ -50,7 +50,7 @@ $autoload['libraries'] = array();
 |	$autoload['helper'] = array('url', 'file');
 */
 
-$autoload['helper'] = array();
+$autoload['helper'] = array('language', 'webconfig');
 
 
 /*
