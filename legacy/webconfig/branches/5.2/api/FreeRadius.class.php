d4862d5325402e6fa15a1d4f08aae307d82255b8
pbaldwin <pbaldwin@d196f2e3-cd94-45c7-a57a-680589916e42>
2010-10-25 19:36:43 +0000

    - Completed most of the implementation [tracker #58]
    git-svn-id: svn://svn.clearfoundation.com/clearos@206 d196f2e3-cd94-45c7-a57a-680589916e42

--- legacy/webconfig/branches/5.2/api/FreeRadius.class.php
+++ legacy/webconfig/branches/5.2/api/FreeRadius.class.php
@@ -38,6 +38,12 @@
 
 require_once('Daemon.class.php');
 
+// FIXME - translate
+define('FREERADIUS_LANG_CLIENT', 'Client');
+define('FREERADIUS_LANG_CLIENTS', 'Clients');
+define('FREERADIUS_LANG_NICKNAME', 'Nickname');
+define('FREERADIUS_LANG_GROUP', 'Group');
+
 ///////////////////////////////////////////////////////////////////////////////
 // C L A S S
 ///////////////////////////////////////////////////////////////////////////////
@@ -55,6 +61,16 @@ require_once('Daemon.class.php');
 class FreeRadius extends Daemon
 {
 	///////////////////////////////////////////////////////////////////////////////
+	// F I E L D S
+	///////////////////////////////////////////////////////////////////////////////
+
+	protected $is_loaded = FALSE;
+	protected $clients = array();
+
+	const FILE_CLIENTS = '/etc/raddb/clearos-clients.conf';
+	const FILE_USERS = '/etc/raddb/clearos-users';
+
+	///////////////////////////////////////////////////////////////////////////////
 	// M E T H O D S
 	///////////////////////////////////////////////////////////////////////////////
 
@@ -70,6 +86,267 @@ class FreeRadius extends Daemon
 		parent::__construct('radiusd');
 
 		require_once(GlobalGetLanguageTemplate(__FILE__));
+	}
+
+	/**
+	 * Adds a client.
+	 *
+	 * @param string $address client address
+	 * @param string $secret client secret
+	 * @param string $nickname client nickname
+	 * @return array clients information
+	 * @throws EngineException
+	 */
+
+	function AddClient($address, $secret, $nickname)
+	{
+		if (COMMON_DEBUG_MODE)
+			self::Log(COMMON_DEBUG, "called", __METHOD__, __LINE__);
+
+		if (! $this->is_loaded)
+			$this->_LoadConfig();
+
+		// FIXME: validation
+
+		if (isset($this->clients[$address]))
+			throw new EngineException(FREERADIUS_LANG_CLIENT . ' - ' . LOCALE_LANG_EXISTS, COMMON_WARNING);
+
+		// FIXME: allowed passwords?  Should it be quoted?
+		$this->clients[$address]['secret'] = $secret;
+		$this->clients[$address]['shortname'] = $nickname;
+
+		$this->_SaveConfig();
+	}
+
+	/**
+	 * Deletes a client.
+	 *
+	 * @param string $address client address
+	 * @return array clients information
+	 * @throws EngineException
+	 */
+
+	function DeleteClient($address)
+	{
+		if (COMMON_DEBUG_MODE)
+			self::Log(COMMON_DEBUG, "called", __METHOD__, __LINE__);
+
+		if (! $this->is_loaded)
+			$this->_LoadConfig();
+
+		if (! isset($this->clients[$address]))
+			throw new EngineException(FREERADIUS_LANG_CLIENT . ' - ' . LOCALE_LANG_INVALID, COMMON_WARNING);
+
+		unset($this->clients[$address]);
+
+		$this->_SaveConfig();
+	}
+
+	/**
+	 * Returns client information for a given nickname.
+	 *
+	 * @param string $address client address
+	 * @return array clients information
+	 * @throws EngineException
+	 */
+
+	function GetClientInfo($address)
+	{
+		if (COMMON_DEBUG_MODE)
+			self::Log(COMMON_DEBUG, "called", __METHOD__, __LINE__);
+
+		if (! $this->is_loaded)
+			$this->_LoadConfig();
+
+		if (!isset($this->clients[$address]))
+			throw new EngineException(FREERADIUS_LANG_CLIENT . ' - ' . LOCALE_LANG_INVALID, COMMON_WARNING);
+
+		return $this->clients[$address];
+	}
+
+	/**
+	 * Returns clients information.
+	 *
+	 * @return array clients information
+	 * @throws EngineException
+	 */
+
+	function GetClients()
+	{
+		if (COMMON_DEBUG_MODE)
+			self::Log(COMMON_DEBUG, "called", __METHOD__, __LINE__);
+
+		if (! $this->is_loaded)
+			$this->_LoadConfig();
+
+		return $this->clients;
+	}
+	/**
+	 * Returns user defined RADIUS group.
+	 *
+	 * @return string user defined RADIUS group
+	 * @throws EngineException
+	 */
+
+	function GetGroup()
+	{
+		if (COMMON_DEBUG_MODE)
+			self::Log(COMMON_DEBUG, "called", __METHOD__, __LINE__);
+
+		$lines = array();
+
+		try {
+			$usersfile = new File(self::FILE_USERS, TRUE);
+			$lines = $usersfile->GetContentsAsArray();
+		} catch (Exception $e) {
+			throw new EngineException($e->GetMessage(), COMMON_WARNING);
+		}
+
+		$group = '';
+
+		foreach ($lines as $line) {
+			$matches = array();
+			if (preg_match('/^DEFAULT LDAP-Group .= "([^\"]*)",/', $line, $matches)) {
+				$group = $matches[1];
+				break;
+			}
+		}
+
+		return $group;
+	}
+
+	/**
+	 * Updates client information.
+	 *
+	 * @param string $address client address
+	 * @param string $secret client secret
+	 * @param string $nickname client nickname
+	 * @return void
+	 * @throws EngineException
+	 */
+
+	function UpdateClient($address, $secret, $nickname)
+	{
+		if (COMMON_DEBUG_MODE)
+			self::Log(COMMON_DEBUG, "called", __METHOD__, __LINE__);
+
+		if (! $this->is_loaded)
+			$this->_LoadConfig();
+
+		if (! isset($this->clients[$address]))
+			throw new EngineException(FREERADIUS_LANG_CLIENT . ' - ' . LOCALE_LANG_INVALID, COMMON_WARNING);
+
+		$this->clients[$address]['shortname'] = $nickname;
+		$this->clients[$address]['secret'] = $secret;
+
+		$this->_SaveConfig();
+	}
+
+	/**
+	 * Updates user defined RADIUS group.
+	 *
+	 * @param string $group user defined RADIUS group
+	 * @return void
+	 * @throws EngineException
+	 */
+
+	function UpdateGroup($group)
+	{
+		if (COMMON_DEBUG_MODE)
+			self::Log(COMMON_DEBUG, "called", __METHOD__, __LINE__);
+
+		try {
+			$file = new File(self::FILE_USERS, TRUE);
+			if ($file->Exists())
+				$file->Delete();
+
+			$file->Create('root', 'radiusd', '0640');
+			$file->AddLines("DEFAULT LDAP-Group != \"$group\", Auth-Type := Reject\n");
+		} catch (Exception $e) {
+			throw new EngineException($e->GetMessage(), COMMON_WARNING);
+		}
+	}
+
+	///////////////////////////////////////////////////////////////////////////////
+	// P R I V A T E  M E T H O D S
+	///////////////////////////////////////////////////////////////////////////////
+
+	/**
+	 * Loads configuration file.
+	 *
+	 * @access private
+	 * @return void
+	 * @throws EngineException
+	 */
+
+	private function _LoadConfig()
+	{
+		if (COMMON_DEBUG_MODE)
+			self::Log(COMMON_DEBUG, "called", __METHOD__, __LINE__);
+
+		$lines = array();
+
+		try {
+			$clientsfile = new File(self::FILE_CLIENTS, TRUE);
+			$lines = $clientsfile->GetContentsAsArray();
+		} catch (Exception $e) {
+			throw new EngineException($e->GetMessage(), COMMON_WARNING);
+		}
+
+		$client = '';
+		$this->clients = array();
+
+		foreach ($lines as $line) {
+			$matches = array();
+
+			if (preg_match('/^\s*client\s*([^\s]+)\s*{/', $line, $matches))
+				$client = $matches[1];
+			else if (preg_match('/^\s*secret\s*=\s*([^\s]+)/', $line, $matches))
+				$this->clients[$client]['secret'] =  $matches[1];
+			else if (preg_match('/^\s*shortname\s*=\s*([^\s]+)/', $line, $matches))
+				$this->clients[$client]['shortname'] =  $matches[1];
+		}
+
+		ksort($this->clients);
+
+		$this->is_loaded = TRUE;
+	}
+
+	/**
+	 * Saves configuration file.
+	 *
+	 * @access private
+	 * @return void
+	 * @throws EngineException
+	 */
+
+	private function _SaveConfig()
+	{
+		if (COMMON_DEBUG_MODE)
+			self::Log(COMMON_DEBUG, "called", __METHOD__, __LINE__);
+
+		$contents = '';
+
+		foreach ($this->clients as $client => $details) {
+			$contents .= "client $client {\n";
+			$contents .= "\tsecret = " . $details['secret'] . "\n";
+			$contents .= "\tshortname = " . $details['shortname'] . "\n";
+			$contents .= "}\n";
+		}
+
+		try {
+			$clientsfile = new File(self::FILE_CLIENTS, TRUE);
+
+			if ($clientsfile->Exists())
+				$clientsfile->Delete();
+
+			$clientsfile->Create('root', 'radiusd', '0640');
+			$clientsfile->AddLines($contents);
+
+			$this->is_loaded = FALSE;
+		} catch (Exception $e) {
+			throw new EngineException($e->GetMessage(), COMMON_WARNING);
+		}
 	}
 
 	/**
--- legacy/webconfig/branches/5.2/htdocs/admin/radius.php
+++ legacy/webconfig/branches/5.2/htdocs/admin/radius.php
@@ -24,6 +24,8 @@
 
 require_once("../../gui/Webconfig.inc.php");
 require_once("../../api/FreeRadius.class.php");
+require_once("../../api/GroupManager.class.php");
+require_once("../../api/Network.class.php");
 require_once(GlobalGetLanguageTemplate(__FILE__));
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -34,7 +36,7 @@ require_once(GlobalGetLanguageTemplate(__FILE__));
 
 WebAuthenticate();
 WebHeader(WEB_LANG_PAGE_TITLE);
-WebDialogIntro(WEB_LANG_PAGE_TITLE, "/images/icon-google-apps.png", WEB_LANG_PAGE_INTRO);
+WebDialogIntro(WEB_LANG_PAGE_TITLE, "/images/icon-radius.png", WEB_LANG_PAGE_INTRO);
 WebCheckUserDatabase();
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -44,6 +46,11 @@ WebCheckUserDatabase();
 ///////////////////////////////////////////////////////////////////////////////
 
 $radius = new FreeRadius();
+$network = new Network(); // Translations only
+
+$secret = isset($_POST['secret']) ? $_POST['secret'] : '';
+$address = isset($_POST['address']) ? $_POST['address'] : '';
+$shortname = isset($_POST['shortname']) ? $_POST['shortname'] : '';
 
 try {
     if (isset($_POST['EnableBoot'])) {
@@ -54,6 +61,25 @@ try {
         $radius->SetRunningState(true);
     } else if (isset($_POST['StopDaemon'])) {
         $radius->SetRunningState(false);
+    } else if (isset($_POST['UpdateConfiguration'])) {
+		$group = isset($_POST['group']) ? $_POST['group'] : '';
+		$radius->UpdateGroup($group);
+		$radius->Reset();
+    } else if (isset($_POST['AddClient'])) {
+		$radius->AddClient($address, $secret, $shortname);
+		$radius->Reset();
+		$address = '';
+		$secret = '';
+		$shortname = '';
+    } else if (isset($_POST['UpdateClient'])) {
+		$radius->UpdateClient($address, $secret, $shortname);
+		$radius->Reset();
+		$address = '';
+		$secret = '';
+		$shortname = '';
+    } else if (isset($_POST['Confirm'])) {
+		$radius->DeleteClient(key($_POST['Confirm']));
+		$radius->Reset();
 	}
 } catch (Exception $e) {
     WebDialogWarning($e->GetMessage());
@@ -65,9 +91,14 @@ try {
 //
 ///////////////////////////////////////////////////////////////////////////////
 
-WebDialogDaemon("radiusd");
-// DisplayConfiguration();
-WebDialogWarning("RADIUS is experimental.  Please see Howto document.");
+if (isset($_POST['EditClient'])) {
+	DisplayEditClient(key($_POST['EditClient']), '', '');
+} else if (isset($_POST['DeleteClient'])) {
+	DisplayConfirmation(key($_POST['DeleteClient']));
+} else {
+	WebDialogDaemon("radiusd");
+	DisplayConfiguration($address, $secret, $shortname);
+}
 Webfooter();
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -80,14 +111,126 @@ Webfooter();
 //
 ///////////////////////////////////////////////////////////////////////////////
 
-function DisplayConfiguration()
+function DisplayConfiguration($address, $secret, $shortname)
 {
 	global $radius;
 
 	try {
+		$group = $radius->GetGroup();
+		$clients = $radius->GetClients();
+
+		$groupmanager = new GroupManager();
+		$grouplist = $groupmanager->GetAllGroups();
 	} catch (Exception $e) {
 		WebDialogWarning($e->GetMessage());
 	}
+
+	$clients_html = '';
+
+	foreach ($clients as $client => $detail) {
+		$clients_html .= "
+			<tr>
+				<td width='170'>" . $detail['shortname'] . "</td>
+				<td>" . $client . "</td>
+				<td>" . $detail['secret'] . "</td>
+				<td>" . 
+					WebButtonEdit("EditClient[" .  $client . "]") . " " . 
+					WebButtonDelete("DeleteClient[" .  $client . "]") . "
+				</td>
+			</tr>
+		";
+	}
+
+	// TODO: what's the expected sorting for RADIUS users?
+
+	WebFormOpen();
+	WebTableOpen(WEB_LANG_PAGE_TITLE);
+	echo "
+		<tr>
+			<td width='170' class='mytablesubheader'>" . FREERADIUS_LANG_GROUP . "</td>
+			<td>" . WebDropDownArray('group', $group, $grouplist) . "</td>
+		</tr>
+		<tr>
+			<td width='170' class='mytablesubheader'>&nbsp; </td>
+			<td>" . WebButtonUpdate('UpdateConfiguration') . "</td>
+		</tr>
+	";
+	WebTableClose();
+	WebFormClose();
+
+
+	WebFormOpen();
+	WebTableOpen(FREERADIUS_LANG_CLIENTS);
+	WebTableHeader(FREERADIUS_LANG_NICKNAME . '|' . NETWORK_LANG_IP . '|' . LOCALE_LANG_PASSWORD . '|' . LOCALE_LANG_ACTION);
+	echo "
+		$clients_html
+		<tr>
+			<td><input type='text' name='shortname' value='$shortname'></td>
+			<td><input type='text' name='address' value='$address'></td>
+			<td><input type='text' name='secret' value='$secret'></td>
+			<td>" . WebButtonAdd("AddClient") . "</td>
+		</tr>
+	";
+	WebTableClose();
+	WebFormClose();
+}
+
+///////////////////////////////////////////////////////////////////////////////
+//
+// DisplayEditClient()
+//
+///////////////////////////////////////////////////////////////////////////////
+
+function DisplayEditClient($address, $shortname, $secret)
+{
+	global $radius;
+
+	try {
+		$details = $radius->GetClientInfo($address);
+	} catch (Exception $e) {
+		WebDialogWarning($e->GetMessage());
+	}
+
+	$shortname = empty($shortname) ? $details['shortname'] : $shortname;
+	$secret = empty($secret) ? $details['secret'] : $secret;
+
+	WebFormOpen();
+	WebTableOpen(FREERADIUS_LANG_CLIENT);
+	echo "
+		<tr>
+			<td class='mytablesubheader'>" . NETWORK_LANG_IP . "</td>
+			<td>$address<input type='hidden' name='address' value='$address'></td>
+		</tr>
+		<tr>
+			<td class='mytablesubheader'>" . FREERADIUS_LANG_NICKNAME . "</td>
+			<td><input type='text' name='shortname' value='" . $details['shortname'] . "'></td>
+		</tr>
+		<tr>
+			<td class='mytablesubheader'>" . LOCALE_LANG_PASSWORD . "</td>
+			<td><input type='text' name='secret' value='" . $details['secret'] . "'></td>
+		</tr>
+		<tr>
+			<td class='mytablesubheader'>" . LOCALE_LANG_PASSWORD . "</td>
+			<td>" . WebButtonUpdate("UpdateClient") . " " . WebButtonCancel("Cancel") . "</td>
+		</tr>
+	";
+	WebTableClose();
+	WebFormClose();
+}
+
+///////////////////////////////////////////////////////////////////////////////
+//
+// DisplayConfirmation()
+//
+///////////////////////////////////////////////////////////////////////////////
+
+function DisplayConfirmation($address)
+{
+	// TODO: translate
+	WebFormOpen();
+	WebDialogWarning("Are you sure you want to delete the following client: $address?" .
+		WebButtonConfirm("Confirm[$address]") . " " .  WebButtonCancel("Cancel"));
+	WebFormClose();
 }
 
 // vi: syntax=php ts=4
