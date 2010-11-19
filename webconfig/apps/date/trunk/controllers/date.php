c3c11b2bc3daca738d3003dd023c625002bf4551
pbaldwin <pbaldwin@d196f2e3-cd94-45c7-a57a-680589916e42>
2010-11-19 04:50:06 +0000

    - Updated to new coding standard
    git-svn-id: svn://svn.clearfoundation.com/clearos@259 d196f2e3-cd94-45c7-a57a-680589916e42

--- webconfig/apps/date/trunk/controllers/date.php
+++ webconfig/apps/date/trunk/controllers/date.php
@@ -6,10 +6,10 @@
 //
 ///////////////////////////////////////////////////////////////////////////////
 //
-// This program is free software; you can redistribute it and/or
-// modify it under the terms of the GNU General Public License
-// as published by the Free Software Foundation; either version 2
-// of the License, or (at your option) any later version.
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
 //
 // This program is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -17,9 +17,8 @@
 // GNU General Public License for more details.
 //
 // You should have received a copy of the GNU General Public License
-// along with this program; if not, write to the Free Software
-// Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
-//
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.  
+//  
 ///////////////////////////////////////////////////////////////////////////////
 
 /**
@@ -27,12 +26,26 @@
  *
  * @package Frontend
  * @author {@link http://www.clearfoundation.com ClearFoundation}
- * @license http://www.gnu.org/copyleft/gpl.html GNU Public License
+ * @license http://www.gnu.org/copyleft/lgpl.html GNU General Public License version 3 or later
  * @copyright Copyright 2010, ClearFoundation
  * @link http://www.clearfoundation.com	
  */
 
-class Date extends MX_Controller 
+///////////////////////////////////////////////////////////////////////////////
+// C L A S S
+///////////////////////////////////////////////////////////////////////////////
+
+/**
+ * Date controller.
+ *
+ * @package Frontend
+ * @author {@link http://www.clearfoundation.com ClearFoundation}
+ * @license http://www.gnu.org/copyleft/lgpl.html GNU General Public License version 3 or later
+ * @copyright Copyright 2010, ClearFoundation
+ * @link http://www.clearfoundation.com	
+ */
+
+class Date extends ClearOS_Controller 
 {
 	/**
 	 * Date default controller
@@ -50,7 +63,7 @@ class Date extends MX_Controller
 		// Handle form submit
 		//-------------------
 
-		if ($this->input->post('submit')) {
+		if ($this->input->post('submit') && ($form_ok)) {
 			try {
 				$this->ntptime->SetTimeZone($this->input->post('timezone'));
 			} catch (Exception $e) {
@@ -77,9 +90,9 @@ class Date extends MX_Controller
 
 		$header['title'] = lang('date_time_and_date');
 
-//		$this->load->view('theme/header', $header);
+		$this->load->view('theme/header', $header);
 		$this->load->view('date', $data);
-//		$this->load->view('theme/footer');
+		$this->load->view('theme/footer');
 	}
 
 	/**
