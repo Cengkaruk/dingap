03ac46530a8ce2393a8b76482a1ae82f725e005e
pbaldwin <pbaldwin@d196f2e3-cd94-45c7-a57a-680589916e42>
2010-11-23 22:58:57 +0000

    - Added and removed some tags
    git-svn-id: svn://svn.clearfoundation.com/clearos@272 d196f2e3-cd94-45c7-a57a-680589916e42

--- webconfig/apps/dhcp/trunk/language/en_US/dhcp_lang.php
+++ webconfig/apps/dhcp/trunk/language/en_US/dhcp_lang.php
@@ -4,7 +4,6 @@ $lang['dhcp_authoritative'] = "Authoritative";
 $lang['dhcp_domain'] = "Domain";
 $lang['dhcp_dns'] = "DNS";
 $lang['dhcp_gateway'] = "Gateway";
-$lang['dhcp_global_configuration'] = 'Global Configuration';
 $lang['dhcp_ip_range_start'] = "IP Range Start";
 $lang['dhcp_ip_range_end'] = "IP Range End";
 $lang['dhcp_lease_time'] = "Lease Time";
@@ -12,7 +11,14 @@ $lang['dhcp_network'] = "Network";
 $lang['dhcp_network_interface'] = "Network Interface";
 $lang['dhcp_ntp'] = "NTP";
 $lang['dhcp_subnet'] = "Subnet";
+$lang['dhcp_subnets'] = "Subnets";
 $lang['dhcp_tftp'] = "TFTP";
 $lang['dhcp_wins'] = "WINS";
+
+$lang['dhcp_leases'] = 'Leases';
+$lang['dhcp_active'] = 'Active';
+$lang['dhcp_dynamic'] = 'Dynamic';
+$lang['dhcp_static'] = 'Static';
+$lang['dhcp_dhcp'] = 'DHCP';
 
 ?>
