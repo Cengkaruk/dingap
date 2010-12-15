38a2f69c9c396ac156fcdf2c026017868440e25c
pbaldwin <pbaldwin@d196f2e3-cd94-45c7-a57a-680589916e42>
2010-12-15 03:30:46 +0000

    - Removed old versions
    git-svn-id: svn://svn.clearfoundation.com/clearos@341 d196f2e3-cd94-45c7-a57a-680589916e42

--- webconfig/themes/clearos6xmobile/trunk/core/head.php
+++ webconfig/themes/clearos6xmobile/trunk/core/head.php
@@ -50,8 +50,7 @@ function page_head($theme_path)
 <link type='text/css' href='$theme_path/css/jquery.mobile-1.0a2.min.css' rel='stylesheet'>
 
 <!-- Theme Javascript -->
-<script type='text/javascript' src='$theme_path/js/jquery.mobile-1.0a1.min.js'></script>
-<!-- script type='text/javascript' src='$theme_path/js/jquery.mobile-1.0a2.min.js'></script -->
+<script type='text/javascript' src='$theme_path/js/jquery.mobile-1.0a2.min.js'></script>
 
 ";
 }
