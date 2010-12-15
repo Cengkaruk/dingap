17f7f51d50348a8ac556c0b025a64106fd865b9b
pbaldwin <pbaldwin@d196f2e3-cd94-45c7-a57a-680589916e42>
2010-12-15 02:44:30 +0000

    - Removed experimental code
    git-svn-id: svn://svn.clearfoundation.com/clearos@333 d196f2e3-cd94-45c7-a57a-680589916e42

--- webconfig/themes/clearos6x/trunk/widgets/head.php
+++ webconfig/themes/clearos6x/trunk/widgets/head.php
@@ -47,16 +47,11 @@ function page_head($theme_path)
 
 <!-- Theme Style Sheets -->
 <link type='text/css' href='$theme_path/css/jquery-ui-1.8.5.custom.css' rel='stylesheet'>
-<link type='text/css' href='$theme_path/css/jquery.ui.selectmenu.css' rel='stylesheet'>
-<link type='text/css' href='$theme_path/css/ui.jqgrid.css' rel='stylesheet'>
 <link type='text/css' href='$theme_path/css/superfish.css' rel='stylesheet'>
 <link type='text/css' href='$theme_path/css/theme.css' rel='stylesheet'>
 
 <!-- Theme Javascript -->
 <script type='text/javascript' src='$theme_path/js/jquery-ui-1.8.5.custom.min.js'></script>
-<script type='text/javascript' src='$theme_path/js/jquery.ui.selectmenu.js'></script>
-<script type='text/javascript' src='$theme_path/js/grid.locale-en.js'></script>
-<script type='text/javascript' src='$theme_path/js/jquery.jqGrid.min.js'></script>
 <script type='text/javascript' src='$theme_path/js/superfish.js'></script>
 <script type='text/javascript' src='$theme_path/js/theme.js'></script>
 
