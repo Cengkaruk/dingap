c27d8a5d16c9ee293c14e1870dfef987a290367f
pbaldwin <pbaldwin@d196f2e3-cd94-45c7-a57a-680589916e42>
2011-01-10 02:49:49 +0000

    - Added menu
    git-svn-id: svn://svn.clearfoundation.com/clearos@431 d196f2e3-cd94-45c7-a57a-680589916e42

---
+++ webconfig/apps/date/trunk/deploy/menu.php
@@ -0,0 +1,7 @@
+<?php
+
+$menu['/app/date']['title'] = menu('date_date');
+$menu['/app/date']['category'] = menu('menu_category_system');
+$menu['/app/date']['subcategory'] = menu('menu_subcategory_settings');
+
+?>
