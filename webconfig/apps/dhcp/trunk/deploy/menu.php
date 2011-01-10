14537e9e3b0b62ae6fb29a44f8f15cf0affeb202
pbaldwin <pbaldwin@d196f2e3-cd94-45c7-a57a-680589916e42>
2011-01-10 02:51:04 +0000

    - Completed menu system
    git-svn-id: svn://svn.clearfoundation.com/clearos@432 d196f2e3-cd94-45c7-a57a-680589916e42

--- webconfig/apps/dhcp/trunk/deploy/menu.php
+++ webconfig/apps/dhcp/trunk/deploy/menu.php
@@ -1,27 +1,5 @@
 <?php
 
-clearos_load_language('date');
-
-$menu['/app/date']['category'] = lang('base_system');
-$menu['/app/date']['subcategory'] = lang('base_settings');
-$menu['/app/date']['title'] = lang('date_date');
-
-// FIXME - testing
-$menu['/app/two']['category'] = lang('base_directory');
-$menu['/app/two']['subcategory'] = 'Subcategory';
-$menu['/app/two']['title'] = 'Test2';
-
-$menu['/app/one']['category'] = lang('base_directory');
-$menu['/app/one']['subcategory'] = 'Subcategory';
-$menu['/app/one']['title'] = 'Test1';
-
-// FIXME - translate
-$menu['/app/dhcp']['category'] = 'Network';
-$menu['/app/dhcp']['subcategory'] = 'Infrastructure';
-$menu['/app/dhcp']['title'] = 'DHCP Server';
-
-$menu['/app/dns']['category'] = 'Network';
-$menu['/app/dns']['subcategory'] = 'Infrastructure';
-$menu['/app/dns']['title'] = 'Local DNS Server';
-
-// vim: ts=4 syntax=php
+$menu['/app/dhcp']['title'] = menu('dhcp_dhcp_server');
+$menu['/app/dhcp']['category'] =  menu('menu_category_network');
+$menu['/app/dhcp']['subcategory'] =  menu('menu_subcategory_infrastructure');
