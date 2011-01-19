8529dc8c12e3d34da09a8dec1361c43221578954
pbaldwin <pbaldwin@d196f2e3-cd94-45c7-a57a-680589916e42>
2011-01-19 03:20:33 +0000

    - Changed due to new naming convention
    git-svn-id: svn://svn.clearfoundation.com/clearos@475 d196f2e3-cd94-45c7-a57a-680589916e42

--- webconfig/apps/filescan/trunk/deploy/menu.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$menu['/app/filescan']['title'] = menu('filescan_antimalware_file_scanner');
-$menu['/app/filescan']['category'] = menu('menu_category_server');
-$menu['/app/filescan']['subcategory'] = menu('menu_subcategory_file_and_print');
-
-?>
