82a8242206932356e45244ef2d3318c67fd96d05
pbaldwin <pbaldwin@d196f2e3-cd94-45c7-a57a-680589916e42>
2011-01-19 20:52:33 +0000

    - Changed menu system to avoid namespace issues
    git-svn-id: svn://svn.clearfoundation.com/clearos@488 d196f2e3-cd94-45c7-a57a-680589916e42

--- webconfig/apps/date/trunk/deploy/menu.php
+++ webconfig/apps/date/trunk/deploy/menu.php
@@ -1,7 +1,7 @@
 <?php
 
-$menu['/app/date']['title'] = menu('date_date');
-$menu['/app/date']['category'] = menu('menu_category_system');
-$menu['/app/date']['subcategory'] = menu('menu_subcategory_settings');
+$menu['/app/date']['title'] = menu('date', 'date_date');
+$menu['/app/date']['category'] = menu('menu', 'menu_category_system');
+$menu['/app/date']['subcategory'] = menu('menu', 'menu_subcategory_settings');
 
 ?>
--- webconfig/apps/dhcp/trunk/deploy/menu.php
+++ webconfig/apps/dhcp/trunk/deploy/menu.php
@@ -1,5 +1,5 @@
 <?php
 
-$menu['/app/dhcp']['title'] = menu('dhcp_dhcp_server');
-$menu['/app/dhcp']['category'] =  menu('menu_category_network');
-$menu['/app/dhcp']['subcategory'] =  menu('menu_subcategory_infrastructure');
+$menu['/app/dhcp']['title'] = menu('dhcp', 'dhcp_dhcp_server');
+$menu['/app/dhcp']['category'] =  menu('menu', 'menu_category_network');
+$menu['/app/dhcp']['subcategory'] =  menu('menu', 'menu_subcategory_infrastructure');
