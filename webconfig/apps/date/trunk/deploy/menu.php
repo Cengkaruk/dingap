0af22323d0255cf44e7fba59d025a7031a969d13
pbaldwin <pbaldwin@d196f2e3-cd94-45c7-a57a-680589916e42>
2011-01-21 19:11:16 +0000

    - Removed trailing php tag
    git-svn-id: svn://svn.clearfoundation.com/clearos@508 d196f2e3-cd94-45c7-a57a-680589916e42

--- webconfig/apps/date/trunk/deploy/menu.php
+++ webconfig/apps/date/trunk/deploy/menu.php
@@ -3,5 +3,3 @@
 $menu['/app/date']['title'] = menu('date', 'date_date');
 $menu['/app/date']['category'] = menu('menu', 'menu_category_system');
 $menu['/app/date']['subcategory'] = menu('menu', 'menu_subcategory_settings');
-
-?>
