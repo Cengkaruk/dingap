9725ea60fb6fbb0cb752ec680e4825ddc7e24489
pbaldwin <pbaldwin@d196f2e3-cd94-45c7-a57a-680589916e42>
2011-01-30 02:52:56 +0000

    - Cleaned up translations
    git-svn-id: svn://svn.clearfoundation.com/clearos@565 d196f2e3-cd94-45c7-a57a-680589916e42

--- webconfig/apps/date/trunk/language/en_US/date_lang.php
+++ webconfig/apps/date/trunk/language/en_US/date_lang.php
@@ -6,11 +6,6 @@ $lang['date_time'] = 'Time';
 $lang['date_time_and_date'] = 'Time and Date';
 $lang['date_time_server'] = 'Time Server';
 $lang['date_time_zone'] = 'Time Zone';
-
-// Excpetions 
-$lang['date_exception_synchronization_failed'] = 'Time synchronization failed.';
 $lang['date_exception_synchronization_schedule_invalid'] = 'Time synchronization schedule is invalid.';
-
-// Validation errors
 $lang['date_validate_time_server_invalid'] = 'Time server is invalid.';
 $lang['date_validate_time_zone_invalid'] = 'Time zone is invalid.';
