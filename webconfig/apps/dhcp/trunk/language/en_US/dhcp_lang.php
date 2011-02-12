298cbaddeacfa8ed8ce6ccc9f72d1ec5c2cdd5b4
pbaldwin <pbaldwin@d196f2e3-cd94-45c7-a57a-680589916e42>
2011-02-12 03:33:29 +0000

    - Added new language tags
    git-svn-id: svn://svn.clearfoundation.com/clearos@658 d196f2e3-cd94-45c7-a57a-680589916e42

--- webconfig/apps/dhcp/trunk/language/en_US/dhcp_lang.php
+++ webconfig/apps/dhcp/trunk/language/en_US/dhcp_lang.php
@@ -23,4 +23,16 @@ $lang['dhcp_dhcp'] = 'DHCP';
 $lang['dhcp_dhcp_server'] = 'DHCP Server';
 $lang['dhcp_confirm_delete'] = 'Are you sure you want to delete DHCP settings for the %s network interface?';
 
-?>
+$lang['dhcp_authoritative_state_invalid'] = 'Authoritative state is invalid.';
+$lang['dhcp_dns_server_invalid'] = 'DNS server is invalid.';
+$lang['dhcp_dns_server_list_invalid'] = 'DNS server list is invalid.';
+$lang['dhcp_domain_invalid'] = 'Domain name is invalid.';
+$lang['dhcp_lease_time_invalid'] = 'Lease time is invalid.';
+$lang['dhcp_start_ip_invalid'] = 'Start IP is invalid.';
+$lang['dhcp_end_ip_invalid'] = 'End IP is invalid.';
+$lang['dhcp_gateway_invalid'] = 'Gateway is invalid.';
+$lang['dhcp_ntp_server_invalid'] = 'NTP server is invalid.';
+$lang['dhcp_tftp_server_invalid'] = 'TFTP server is invalid.';
+$lang['dhcp_wins_server_invalid'] = 'WINS server is invalid.';
+$lang['dhcp_network_interface_invalid'] = 'Network interface is invalid.';
+$lang['dhcp_ip_range_invalid'] = 'IP range is invalid.';
