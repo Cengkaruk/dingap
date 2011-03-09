7524de8220c019bdeeab4bb5fb2bdadb536e1c65
pbaldwin <pbaldwin@d196f2e3-cd94-45c7-a57a-680589916e42>
2011-03-09 22:48:51 +0000

    - Continued implementation
    git-svn-id: svn://svn.clearfoundation.com/clearos@714 d196f2e3-cd94-45c7-a57a-680589916e42

---
+++ webconfig/apps/openldap/trunk/config/config.php
@@ -0,0 +1,5 @@
+mode = master
+base_dn = dc=test,dc=lan
+bind_dn = cn=manager,ou=Internal,dc=test,dc=lan
+bind_pw = password
+bind_pw_hash = {SSHA}wLvm6/S9zVaWb1b4yB+xAirEBjwN/MOX
---
+++ webconfig/apps/openldap/trunk/language/en_US/openldap_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['openldap_operation_failed'] = 'Directory operation failed.';
+$lang['openldap_directory_is_unavailable'] = 'Directory is unavailable.';
+$lang['openldap_security_policy_is_invalid'] = 'Security policy is invalid.';
