9bd00562d4c4fe678cb444a8ed7c9019d208da0c
pbaldwin <pbaldwin@d196f2e3-cd94-45c7-a57a-680589916e42>
2011-03-09 22:51:30 +0000

    - Moved group hooks to extension
    git-svn-id: svn://svn.clearfoundation.com/clearos@718 d196f2e3-cd94-45c7-a57a-680589916e42

---
+++ webconfig/apps/groups/trunk/libraries/OpenLDAP_User_Extension.php
@@ -0,0 +1,136 @@
+<?php
+
+/**
+ * Group OpenLDAP user extension.
+ *
+ * @category   Apps
+ * @package    Groups
+ * @subpackage Libraries
+ * @author     ClearFoundation <developer@clearfoundation.com>
+ * @copyright  2011 ClearFoundation
+ * @license    http://www.gnu.org/copyleft/lgpl.html GNU Lesser General Public License version 3 or later
+ * @link       http://www.clearfoundation.com/docs/developer/apps/kolab/
+ */
+
+///////////////////////////////////////////////////////////////////////////////
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+///////////////////////////////////////////////////////////////////////////////
+
+///////////////////////////////////////////////////////////////////////////////
+// N A M E S P A C E
+///////////////////////////////////////////////////////////////////////////////
+
+namespace clearos\apps\groups;
+
+///////////////////////////////////////////////////////////////////////////////
+// B O O T S T R A P
+///////////////////////////////////////////////////////////////////////////////
+
+$bootstrap = getenv('CLEAROS_BOOTSTRAP') ? getenv('CLEAROS_BOOTSTRAP') : '/usr/clearos/framework/shared';
+require_once $bootstrap . '/bootstrap.php';
+
+///////////////////////////////////////////////////////////////////////////////
+// T R A N S L A T I O N S
+///////////////////////////////////////////////////////////////////////////////
+
+clearos_load_language('base');
+
+///////////////////////////////////////////////////////////////////////////////
+// D E P E N D E N C I E S
+///////////////////////////////////////////////////////////////////////////////
+
+// Classes
+//--------
+
+use \clearos\apps\base\Engine as Engine;
+use \clearos\apps\openldap\Utilities as Utilities;
+
+clearos_load_library('base/Engine');
+clearos_load_library('openldap/Utilities');
+
+///////////////////////////////////////////////////////////////////////////////
+// C L A S S
+///////////////////////////////////////////////////////////////////////////////
+
+/**
+ * Group OpenLDAP user extension.
+ *
+ * @category   Apps
+ * @package    Groups
+ * @subpackage Libraries
+ * @author     ClearFoundation <developer@clearfoundation.com>
+ * @copyright  2011 ClearFoundation
+ * @license    http://www.gnu.org/copyleft/lgpl.html GNU Lesser General Public License version 3 or later
+ * @link       http://www.clearfoundation.com/docs/developer/apps/kolab/
+ */
+
+class OpenLDAP_User_Extension extends Engine
+{
+    ///////////////////////////////////////////////////////////////////////////////
+    // C O N S T A N T S
+    ///////////////////////////////////////////////////////////////////////////////
+
+    ///////////////////////////////////////////////////////////////////////////////
+    // V A R I A B L E S
+    ///////////////////////////////////////////////////////////////////////////////
+
+    ///////////////////////////////////////////////////////////////////////////////
+    // M E T H O D S
+    ///////////////////////////////////////////////////////////////////////////////
+
+    /**
+     * Groups OpenLDAP_User_Extension constructor.
+     */
+
+    public function __construct()
+    {
+        clearos_profile(__METHOD__, __LINE__);
+    }
+
+    /**
+     * Runs delete procedure.
+     */
+
+    public function delete_hook()
+    {
+        clearos_profile(__METHOD__, __LINE__);
+
+        // Delete the user from all the groups
+        /*
+        FIXME: revisit
+        $groupmanager = new GroupManager();
+        $groupmanager->DeleteGroupMemberships($this->username);
+        */
+    }
+
+    /**
+     * Runs after adding a user.
+     *
+     * @return void
+     */
+
+    public function add_post_processing_hook()
+    {
+        clearos_profile(__METHOD__, __LINE__);
+
+        // Initialize default group memberships
+        /*
+        // FIXME: revisit
+        $groupmanager = new GroupManager();
+        $groupmanager->InitalizeGroupMemberships($this->username);
+        */
+    }
+}
