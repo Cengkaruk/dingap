0d5dbe1886ef05b9e145fe61a9977280e5b321c8
pbaldwin <pbaldwin@d196f2e3-cd94-45c7-a57a-680589916e42>
2011-03-12 03:12:35 +0000

    - Changed menu sub-category
    git-svn-id: svn://svn.clearfoundation.com/clearos@739 d196f2e3-cd94-45c7-a57a-680589916e42

--- webconfig/apps/greylisting/trunk/deploy/menu.php
+++ webconfig/apps/greylisting/trunk/deploy/menu.php
@@ -2,4 +2,4 @@
 
 $menu['/app/greylisting']['title'] = menu('greylisting', 'greylisting_greylisting');
 $menu['/app/greylisting']['category'] = menu('menu', 'menu_category_gateway');
-$menu['/app/greylisting']['subcategory'] = menu('menu', 'menu_subcategory_mail');
+$menu['/app/greylisting']['subcategory'] = menu('menu', 'menu_subcategory_mail_scanning');
