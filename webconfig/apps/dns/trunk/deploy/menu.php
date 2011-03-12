f9f2593fe89a96c73e65df1eda7d30c0a18bdb18
pbaldwin <pbaldwin@d196f2e3-cd94-45c7-a57a-680589916e42>
2011-03-12 03:39:12 +0000

    - Changed title
    git-svn-id: svn://svn.clearfoundation.com/clearos@743 d196f2e3-cd94-45c7-a57a-680589916e42

--- webconfig/apps/dns/trunk/deploy/menu.php
+++ webconfig/apps/dns/trunk/deploy/menu.php
@@ -1,6 +1,6 @@
 <?php
 
-$menu['/app/dns']['title'] = menu('dns', 'dns_local_dns_server');
+$menu['/app/dns']['title'] = menu('dns', 'dns_dns_server');
 $menu['/app/dns']['category'] = menu('menu', 'menu_category_network');
 $menu['/app/dns']['subcategory'] = menu('menu', 'menu_subcategory_infrastructure');
 
