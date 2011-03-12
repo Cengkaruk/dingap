c46bf3f574787abfbcf35ff38489d2fc0852900b
pbaldwin <pbaldwin@d196f2e3-cd94-45c7-a57a-680589916e42>
2011-03-12 03:12:04 +0000

    - Changed menu item
    git-svn-id: svn://svn.clearfoundation.com/clearos@738 d196f2e3-cd94-45c7-a57a-680589916e42

--- webconfig/apps/imap/trunk/deploy/menu.php
+++ webconfig/apps/imap/trunk/deploy/menu.php
@@ -1,5 +1,5 @@
 <?php
 
-$menu['/app/imap']['title'] = menu('imap', 'imap_imap');
+$menu['/app/imap']['title'] = menu('imap', 'imap_imap_and_pop_server');
 $menu['/app/imap']['category'] = menu('menu', 'menu_category_server');
 $menu['/app/imap']['subcategory'] = menu('menu', 'menu_subcategory_mail');
