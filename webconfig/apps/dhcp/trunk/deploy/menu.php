b0a456cb9fb74236c31acc7655724b4fc67fdcf6
pbaldwin <pbaldwin@d196f2e3-cd94-45c7-a57a-680589916e42>
2011-03-15 04:29:44 +0000

    - Removed old menu item
    git-svn-id: svn://svn.clearfoundation.com/clearos@769 d196f2e3-cd94-45c7-a57a-680589916e42

--- webconfig/apps/dhcp/trunk/deploy/menu.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-$menu['/app/dhcp']['title'] = menu('dhcp', 'dhcp_dhcp_server');
-$menu['/app/dhcp']['category'] =  menu('menu', 'menu_category_network');
-$menu['/app/dhcp']['subcategory'] =  menu('menu', 'menu_subcategory_infrastructure');
