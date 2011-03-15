311cbe6194b84878740a4272002861f226d52ccb
pbaldwin <pbaldwin@d196f2e3-cd94-45c7-a57a-680589916e42>
2011-03-15 04:33:06 +0000

    - Removed old menu configuration
    git-svn-id: svn://svn.clearfoundation.com/clearos@775 d196f2e3-cd94-45c7-a57a-680589916e42

--- webconfig/apps/dns/trunk/deploy/menu.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-$menu['/app/dns']['title'] = menu('dns', 'dns_dns_server');
-$menu['/app/dns']['category'] = menu('menu', 'menu_category_network');
-$menu['/app/dns']['subcategory'] = menu('menu', 'menu_subcategory_infrastructure');
-
--- webconfig/apps/password_policies/trunk/deploy/menu.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-$menu['/app/password_policies']['title'] = menu('password_policies', 'password_policies_password_policies');
-$menu['/app/password_policies']['category'] = menu('menu', 'menu_category_system');
-$menu['/app/password_policies']['subcategory'] = menu('menu', 'menu_subcategory_accounts');
