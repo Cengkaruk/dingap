f7f2b2737997299734f343ce856bcd1d3db86211
pbaldwin <pbaldwin@d196f2e3-cd94-45c7-a57a-680589916e42>
2011-03-15 04:28:03 +0000

    - Removed old menu item
    git-svn-id: svn://svn.clearfoundation.com/clearos@767 d196f2e3-cd94-45c7-a57a-680589916e42

--- webconfig/apps/groups/trunk/deploy/menu.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-$menu['/app/groups']['title'] = menu('groups', 'groups_groups');
-$menu['/app/groups']['category'] = menu('menu', 'menu_category_system');
-$menu['/app/groups']['subcategory'] = menu('menu', 'menu_subcategory_accounts');
