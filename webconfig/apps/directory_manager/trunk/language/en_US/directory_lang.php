c04bf6bcdf6c784d808822b2f349def2f4c6600b
pbaldwin <pbaldwin@d196f2e3-cd94-45c7-a57a-680589916e42>
2011-03-29 17:09:36 +0000

    - Changed app name
    git-svn-id: svn://svn.clearfoundation.com/clearos@852 d196f2e3-cd94-45c7-a57a-680589916e42

---
+++ webconfig/apps/directory_manager/trunk/language/en_US/directory_lang.php
@@ -0,0 +1,137 @@
+<?php
+
+///////////////////////////////////////
+// From LDAP class
+///////////////////////////////////////
+// Change ldap to directory
+
+$lang['directory_master_server'] = 'Master Server';
+$lang['directory_server_mode'] = 'Server Mode';
+
+// Reset
+$lang['directory_errmsg_connection_failed'] = 'Connection to directory server failed.';
+
+// Delete
+// $lang['directory_directory_unavailable'] = 'Directory is unavailable.';
+// $lang['directory_bind_policy'] = 'Security Policy';
+// $lang['directory_errmsg_function_failed'] = 'LDAP function failed';
+
+// Keep
+$lang['directory_base_dn'] = 'LDAP Base DN';
+$lang['directory_bind_dn'] = 'LDAP Bind DN';
+$lang['directory_bind_password'] = 'LDAP Bind Password';
+
+// New
+$lang['directory_security_policy'] = 'Security Policy';
+
+$lang['directory_validate_security_policy_invalid'] = 'Security policy is invalid.';
+
+$lang['directory_exception_directory_is_unavailable'] = 'Directory is unavailable.';
+$lang['directory_exception_function_failed'] = 'LDAP function failed';
+$lang['directory_exception_username_already_exists'] = 'Username already exists.';
+$lang['directory_exception_full_name_already_exists'] = 'A user with the same full name already exists.';
+$lang['directory_exception_user_not_found'] = 'User not found.';
+
+$lang['directory_directory_manager'] = 'Directory Manager';
+
+///////////////////////////////////////
+// From ClearDirectory class
+///////////////////////////////////////
+// Change cleardirectory to directory
+
+// Delete
+// $lang['directory_alias_already_exists'] = 'Alias already exists.';
+// $lang['directory_group_already_exists'] = 'Group already exists.';
+// $lang['directory_username_already_exists'] = 'Username already exists.';
+// $lang['directory_replicate'] = 'Replicate';
+
+// Keep
+$lang['directory_mode'] = 'Mode';
+$lang['directory_master'] = 'Master';
+$lang['directory_slave'] = 'Slave';
+$lang['directory_standalone'] = 'Standalone';
+
+///////////////////////////////////////
+// From User class
+///////////////////////////////////////
+
+// New 
+$lang['directory_validate_username_invalid'] = 'Username is invalid.';
+$lang['directory_validate_username_reserved'] = 'Username is reserved for the system.';
+$lang['directory_validate_user_info_invalid'] = 'User information is invalid.';
+
+$lang['directory_validate_city_invalid'] = 'City is invalid.';
+$lang['directory_validate_country_invalid'] = 'Country is invalid.';
+$lang['directory_validate_description_invalid'] = 'Description is invalid.';
+$lang['directory_validate_display_name_invalid'] = 'Display name is invalid.';
+$lang['directory_validate_fax_invalid'] = 'Fax number is invalid.';
+$lang['directory_validate_first_name_invalid'] = 'First name is invalid.';
+$lang['directory_validate_gid_number_invalid'] = 'Group ID is invalid.';
+$lang['directory_validate_home_directory_invalid'] = 'Home directory is invalid.';
+$lang['directory_validate_last_name_invalid'] = 'Last name is invalid.';
+
+// Keep
+$lang['directory_last_name'] = 'Last Name';
+
+
+// Delete
+// $lang['directory_errmsg_reserved_system_user'] = 'This account name is reserved for the system.';
+
+// TODO
+$lang['directory_account_locked'] = 'Locked';
+$lang['directory_account_lock_status'] = 'Account Lock Status';
+$lang['directory_account_unlocked'] = 'Unlocked';
+$lang['directory_check_first_last_name'] = 'Check the first and last name';
+$lang['directory_display_name'] = 'Display Name';
+$lang['directory_email'] = 'Mailbox';
+$lang['directory_errmsg_access_control_violation'] = 'Access control violation.';
+$lang['directory_errmsg_duplicate_full_name'] = 'User manager must not contain duplicate full names';
+$lang['directory_errmsg_flexshare_exists'] = 'Delete failed - the following Flexshare depends on this user';
+$lang['directory_errmsg_flexshare_with_this_name_exists'] = 'Username is already in use by Flexshare system';
+$lang['directory_errmsg_password_error'] = 'An error occurred setting the password.';
+$lang['directory_errmsg_username_not_exist'] = 'User does not exist.';
+$lang['directory_errmsg_user_already_exists'] = 'User already exists';
+$lang['directory_errmsg_user_not_found'] = 'User not found';
+$lang['directory_errmsg_user_synchronization_error'] = 'User database synchronization error.';
+$lang['directory_extension'] = 'Extension';
+$lang['directory_first_name'] = 'First Name';
+$lang['directory_ftp'] = 'FTP';
+$lang['directory_fullname'] = 'Full Name';
+$lang['directory_gidnumber'] = 'Group ID Number';
+$lang['directory_google_apps'] = 'Google Apps';
+$lang['directory_homedir'] = 'Home Directory';
+$lang['directory_lock'] = 'Lock';
+$lang['directory_mail_address'] = 'E-mail Address';
+$lang['directory_mail_alias'] = 'Mail Alias';
+$lang['directory_mail_aliases'] = 'Mail Aliases';
+$lang['directory_mail_forwarder'] = 'Mail Forwarder';
+$lang['directory_mail_forwarders'] = 'Mail Forwarders';
+$lang['directory_mail_services'] = 'Mail Services';
+$lang['directory_name'] = 'Name';
+$lang['directory_old_password_invalid'] = 'Old password invalid';
+$lang['directory_openvpn'] = 'OpenVPN';
+$lang['directory_options'] = 'Options';
+$lang['directory_password_in_history'] = 'Password is in history of old passwords';
+$lang['directory_password_not_changed'] = 'Password is not being changed from existing value';
+$lang['directory_password_too_young'] = 'Password is too young to change';
+$lang['directory_password_violates_quality_check'] = 'Password fails quality checking policy';
+$lang['directory_password_was_reset'] = 'Password required a reset.';
+$lang['directory_pbx'] = 'PBX';
+$lang['directory_phone_numbers'] = 'Telephone Numbers';
+$lang['directory_pptp'] = 'PPTP VPN';
+$lang['directory_presence'] = 'Presence';
+$lang['directory_proxy'] = 'Proxy Server';
+$lang['directory_quota'] = 'Quota';
+$lang['directory_samba'] = 'Windows Networking';
+$lang['directory_secure_password'] = 'Secure Password';
+$lang['directory_server'] = 'Server';
+$lang['directory_services'] = 'Services';
+$lang['directory_shell'] = 'Login Shell';
+$lang['directory_status_converted'] = 'Converted';
+$lang['directory_status_exists'] = 'Exists';
+$lang['directory_title'] = 'Title';
+$lang['directory_uidnumber'] = 'User ID Number';
+$lang['directory_unlock'] = 'Unlock';
+$lang['directory_user_details'] = 'User Details';
+$lang['directory_verify'] = 'Verify';
+$lang['directory_web'] = 'Web';
