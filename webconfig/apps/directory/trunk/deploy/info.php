c1d18b59f01fe7df93cfb5277f99bbb4c3cbfb85
pbaldwin <pbaldwin@d196f2e3-cd94-45c7-a57a-680589916e42>
2011-03-29 14:49:51 +0000

    - Added basic build details
    git-svn-id: svn://svn.clearfoundation.com/clearos@851 d196f2e3-cd94-45c7-a57a-680589916e42

---
+++ webconfig/apps/directory/trunk/deploy/info.php
@@ -0,0 +1,18 @@
+<?php
+
+$app['basename'] = 'directory';
+$app['version'] = '6.0.0';
+$app['release'] = '1';
+$app['vendor'] = 'ClearFoundation';
+$app['packager'] = 'ClearFoundation';
+$app['license'] = 'GPLv3';
+$app['license_core'] = 'LGPLv3';
+$app['summary'] = 'Directory management and setup.'; // FIXME: translate
+$app['description'] = 'The Directory Manager provides... blah blah blah'; // FIXME: translate
+
+$app['name'] = lang('directory_directory_manager');
+$app['category'] = lang('base_category_server');
+$app['subcategory'] = lang('base_subcategory_directory');
+
+// Packaging
+$app['core_dependencies'] = array('app-base');
