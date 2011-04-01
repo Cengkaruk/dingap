ea0d4158048662fa0501b82a332fef72e9fddf9c
pbaldwin <pbaldwin@d196f2e3-cd94-45c7-a57a-680589916e42>
2011-04-01 19:53:28 +0000

    - Updated packaging
    git-svn-id: svn://svn.clearfoundation.com/clearos@883 d196f2e3-cd94-45c7-a57a-680589916e42

---
+++ webconfig/apps/cron/trunk/deploy/info.php
@@ -0,0 +1,20 @@
+<?php
+
+$app['basename'] = 'cron';
+$app['version'] = '6.0';
+$app['release'] = '0.5';
+$app['vendor'] = 'ClearFoundation';
+$app['packager'] = 'ClearFoundation';
+$app['license'] = 'GPLv3';
+$app['license_core'] = 'LGPLv3';
+$app['summary'] = 'Cron...'; // FIXME
+$app['description'] = 'Cron...'; // FIXME: translate
+
+/*
+$app['name'] = lang('date_time_and_date');
+$app['category'] = lang('base_category_system');
+$app['subcategory'] = lang('base_subcategory_settings');
+*/
+
+// Packaging
+$app['core_dependencies'] = array('app-base-core', 'cronie >= 1.4.4');
