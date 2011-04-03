9285a7242b55a88d7413b6837286f6472b1bb681
pbaldwin <pbaldwin@d196f2e3-cd94-45c7-a57a-680589916e42>
2011-04-03 03:17:23 +0000

    - Added translations
    git-svn-id: svn://svn.clearfoundation.com/clearos@889 d196f2e3-cd94-45c7-a57a-680589916e42

--- webconfig/apps/groups/trunk/language/en_US/groups_lang.php
+++ webconfig/apps/groups/trunk/language/en_US/groups_lang.php
@@ -2,7 +2,8 @@
 $lang['groups_all'] = 'All';
 $lang['groups_builtin_groups'] = 'Built-in Groups';
 $lang['groups_edit'] = 'Edit Group';
-$lang['groups_gid'] = 'Group';
+$lang['groups_group_id'] = 'Group ID';
+$lang['groups_description'] = 'Description';
 $lang['groups_group'] = 'Group';
 $lang['groups_members'] = 'Group Members';
 $lang['groups_system'] = 'System';
@@ -11,3 +12,14 @@ $lang['groups_user'] = 'User';
 $lang['groups_user_defined_groups'] = 'User Defined Groups';
 
 $lang['groups_groups'] = 'Groups';
+
+$lang['groups_group_not_found'] = 'Group not found.';
+$lang['groups_group_already_exists'] = 'Group already exists.';
+$lang['groups_group_is_reserved_for_windows'] = 'Group is reserved for Windows integration.';
+$lang['groups_group_is_reserved_for_system'] = 'Group is reserved for the system.';
+
+$lang['groups_description_is_invalid'] = 'Description is invalid.'; 
+$lang['groups_group_name_is_invalid'] = 'Group name is invalid.'; 
+$lang['groups_username_does_not_exist'] = 'Username does not exist.';
+
+
