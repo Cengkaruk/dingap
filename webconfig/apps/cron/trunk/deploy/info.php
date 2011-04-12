ba37084ea83a4e40a527681bc5c2d1bdc1471fc8
pbaldwin <pbaldwin@d196f2e3-cd94-45c7-a57a-680589916e42>
2011-04-12 11:15:04 +0000

    - Enabled categories
    git-svn-id: svn://svn.clearfoundation.com/clearos@940 d196f2e3-cd94-45c7-a57a-680589916e42

--- webconfig/apps/cron/trunk/deploy/info.php
+++ webconfig/apps/cron/trunk/deploy/info.php
@@ -10,11 +10,9 @@ $app['license_core'] = 'LGPLv3';
 $app['summary'] = 'Cron...'; // FIXME
 $app['description'] = 'Cron...'; // FIXME: translate
 
-/*
-$app['name'] = lang('date_time_and_date');
+$app['name'] = 'Task Scheduler'; // FIXME translate
 $app['category'] = lang('base_category_system');
 $app['subcategory'] = lang('base_subcategory_settings');
-*/
 
 // Packaging
 $app['core_dependencies'] = array('app-base-core', 'cronie >= 1.4.4');
