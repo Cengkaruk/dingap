0fcf2f357dc63580d5a266d2f83957ae98fdb43c
abylund <abylund@d196f2e3-cd94-45c7-a57a-680589916e42>
2011-04-13 01:54:13 +0000

    - Continued development
    git-svn-id: svn://svn.clearfoundation.com/clearos@960 d196f2e3-cd94-45c7-a57a-680589916e42

---
+++ webconfig/apps/graphical_console/trunk/views/shutdown.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ * Graphical console shutdown view.
+ *
+ * @category   ClearOS
+ * @package    Graphical_Console
+ * @subpackage Views
+ * @author     ClearFoundation <developer@clearfoundation.com>
+ * @copyright  2011 ClearFoundation
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU General Public License version 3 or later
+ * @link       http://www.clearfoundation.com/docs/developer/apps/graphical_console/
+ */
+
+///////////////////////////////////////////////////////////////////////////////
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.  
+//  
+///////////////////////////////////////////////////////////////////////////////
+
+$this->lang->load('base');
+
+echo infobox_warning(lang('base_shutdown'), lang('graphical_console_shutting_down_graphical_console'));
