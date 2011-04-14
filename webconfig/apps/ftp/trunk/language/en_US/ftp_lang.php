56627acdd5eaf3f82c2d8d430349858f9692259b
pbaldwin <pbaldwin@d196f2e3-cd94-45c7-a57a-680589916e42>
2011-04-14 19:45:25 +0000

    - Added translations
    git-svn-id: svn://svn.clearfoundation.com/clearos@995 d196f2e3-cd94-45c7-a57a-680589916e42

---
+++ webconfig/apps/ftp/trunk/language/en_US/ftp_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['ftp_ftp_server'] = 'FTP Server';
+$lang['ftp_server_name_is_invalid'] = 'Server name is invalid.';
+$lang['ftp_maximum_instances_is_invalid'] = 'Maximum instances is invalid.';
+$lang['ftp_port_is_invalid'] = 'Server name is invalid.';
