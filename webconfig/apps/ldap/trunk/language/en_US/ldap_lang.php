1dc4465d0bb3687e89f04285c4329ed5bd324eba
pbaldwin <pbaldwin@d196f2e3-cd94-45c7-a57a-680589916e42>
2011-04-16 01:57:41 +0000

    - Added translations
    git-svn-id: svn://svn.clearfoundation.com/clearos@1013 d196f2e3-cd94-45c7-a57a-680589916e42

--- webconfig/apps/ldap/trunk/language/en_US/ldap_lang.php
+++ webconfig/apps/ldap/trunk/language/en_US/ldap_lang.php
@@ -2,3 +2,9 @@
 
 $lang['ldap_ldap_operation_failed'] = 'LDAP Operation failed.';
 $lang['ldap_ldap_is_unavailable'] = 'LDAP is unavailable';
+
+// Keep
+$lang['ldap_mode'] = 'Mode';
+$lang['ldap_master'] = 'Master';
+$lang['ldap_slave'] = 'Slave'; 
+$lang['ldap_standalone'] = 'Standalone';
