bd5ff1ae51a5767fed25c2a20faa8b84f92c7e2c
pbaldwin <pbaldwin@d196f2e3-cd94-45c7-a57a-680589916e42>
2011-04-22 01:47:13 +0000

    - Moved
    git-svn-id: svn://svn.clearfoundation.com/clearos@1071 d196f2e3-cd94-45c7-a57a-680589916e42

--- webconfig/apps/openldap_directory/trunk/language/en_US/openldap_directory_lang.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-$lang['openldap_operation_failed'] = 'Directory operation failed.';
-$lang['openldap_directory_is_unavailable'] = 'Directory is unavailable.';
-$lang['openldap_security_policy_is_invalid'] = 'Security policy is invalid.';
-$lang['openldap_domain_is_invalid'] = 'Domain is invalid.';
