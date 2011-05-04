5ef144efeea68d2fcee6d896713633e2a37171b6
pbaldwin <pbaldwin@d196f2e3-cd94-45c7-a57a-680589916e42>
2011-05-04 01:42:20 +0000

    - Removed field set
    git-svn-id: svn://svn.clearfoundation.com/clearos@1144 d196f2e3-cd94-45c7-a57a-680589916e42

--- webconfig/apps/date/trunk/views/date.php
+++ webconfig/apps/date/trunk/views/date.php
@@ -41,17 +41,15 @@ $this->lang->load('date');
 ///////////////////////////////////////////////////////////////////////////////
 
 echo form_open('date');
-echo form_header(lang('date_time_and_date'));
+echo form_header(lang('base_settings'));
 
 ///////////////////////////////////////////////////////////////////////////////
 // Form Fields and Buttons
 ///////////////////////////////////////////////////////////////////////////////
 
-echo form_fieldset(lang('base_general_settings'));
 echo field_input('date', $date, lang('date_date'), TRUE);
 echo field_input('time', $time, lang('date_time'), TRUE);
 echo field_simple_dropdown('time_zone', $time_zones, $time_zone, lang('date_time_zone'));
-echo form_fieldset_close();
 
 echo button_set(
     array( 
