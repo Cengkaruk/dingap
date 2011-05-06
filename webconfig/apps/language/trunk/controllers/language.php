3098b989af511ef10d18f24db94b236b09c41919
pbaldwin <pbaldwin@d196f2e3-cd94-45c7-a57a-680589916e42>
2011-05-06 16:57:14 +0000

    - Added language app
    git-svn-id: svn://svn.clearfoundation.com/clearos@1201 d196f2e3-cd94-45c7-a57a-680589916e42

---
+++ webconfig/apps/language/trunk/controllers/language.php
@@ -0,0 +1,102 @@
+<?php
+
+/**
+ * Language controller.
+ *
+ * @category   Apps
+ * @package    Language
+ * @subpackage Controllers
+ * @author     ClearFoundation <developer@clearfoundation.com>
+ * @copyright  2011 ClearFoundation
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU General Public License version 3 or later
+ * @link       http://www.clearfoundation.com/docs/developer/apps/language/
+ */
+
+///////////////////////////////////////////////////////////////////////////////
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+///////////////////////////////////////////////////////////////////////////////
+
+///////////////////////////////////////////////////////////////////////////////
+// C L A S S
+///////////////////////////////////////////////////////////////////////////////
+
+/**
+ * Language controller.
+ *
+ * @category   Apps
+ * @package    Language
+ * @subpackage Controllers
+ * @author     ClearFoundation <developer@clearfoundation.com>
+ * @copyright  2011 ClearFoundation
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU General Public License version 3 or later
+ * @link       http://www.clearfoundation.com/docs/developer/apps/language/
+ */
+
+class Language extends ClearOS_Controller
+{
+    /**
+     * Language default controller
+     *
+     * @return view
+     */
+
+    function index()
+    {
+        // Load dependencies
+        //------------------
+
+        $this->load->library('language/Locale');
+        $this->lang->load('language');
+
+        // Set validation rules
+        //---------------------
+         
+        $this->form_validation->set_policy('code', 'language/Locale', 'validate_language_code', TRUE);
+        $form_ok = $this->form_validation->run();
+
+        // Handle form submit
+        //-------------------
+
+        if (($this->input->post('submit') && $form_ok)) {
+            try {
+                $this->locale->set_locale($this->input->post('code'));
+                $this->page->set_status_updated();
+
+                // Do a full reload with the new language setting!
+                redirect('/language');
+            } catch (Exception $e) {
+                $this->page->view_exception($e);
+                return;
+            }
+        }
+
+        // Load view data
+        //---------------
+
+        try {
+            $data['code'] = $this->locale->get_language_code();
+            $data['languages'] = $this->locale->get_languages();
+        } catch (Exception $e) {
+            $this->page->view_exception($e);
+            return;
+        }
+
+        // Load views
+        //-----------
+
+        $this->page->view_form('language', $data, lang('language_language'));
+    }
+}
--- webconfig/apps/language/trunk/language/fr_FR/language_lang.php
+++ webconfig/apps/language/trunk/language/fr_FR/language_lang.php
@@ -2,7 +2,7 @@
 
 $lang['language_app_long_description'] = 'Language long description'; // FIXME
 $lang['language_app_summary'] = 'Language summary'; // FIXME
-$lang['language_language'] = 'Language';
+$lang['language_language'] = 'Langue';
 $lang['language_keyboard'] = 'Keyboard';
 $lang['language_keyboard_is_invalid'] = 'Keyboard is invalid.';
 $lang['language_language_is_invalid'] = 'Language is invalid.';
