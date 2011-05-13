78db20832e8213ee8e3badc2dfdd43aa3343ea81
pbaldwin <pbaldwin@d196f2e3-cd94-45c7-a57a-680589916e42>
2011-05-13 19:21:22 +0000

    - Added organization app
    git-svn-id: svn://svn.clearfoundation.com/clearos@1270 d196f2e3-cd94-45c7-a57a-680589916e42

---
+++ webconfig/apps/organization/trunk/language/en_US/organization_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['organization_app_summary'] = 'Organization app summary';
+$lang['organization_app_long_description'] = 'Organization long description';
+$lang['organization_organization'] = 'Organization';
