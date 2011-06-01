afb5ae7b6ee8f89e0c221fd16c631c85398c9db6
bchambers <bchambers@d196f2e3-cd94-45c7-a57a-680589916e42>
2011-06-01 23:42:38 +0000

    - Added to SVN
    git-svn-id: svn://svn.clearfoundation.com/clearos@1466 d196f2e3-cd94-45c7-a57a-680589916e42

---
+++ webconfig/apps/mail_notification/trunk/controllers/mail_notification.php
@@ -0,0 +1,132 @@
+<?php
+
+/**
+ * Mail Notification controller.
+ *
+ * @category   Apps
+ * @package    Mail_Notification
+ * @subpackage Controllers
+ * @author     ClearFoundation <developer@clearfoundation.com>
+ * @copyright  2011 ClearFoundation
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU General Public License version 3 or later
+ * @link       http://www.clearfoundation.com/docs/developer/apps/raid/
+ */
+
+///////////////////////////////////////////////////////////////////////////////
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+///////////////////////////////////////////////////////////////////////////////
+
+///////////////////////////////////////////////////////////////////////////////
+// D E P E N D E N C I E S
+///////////////////////////////////////////////////////////////////////////////
+
+///////////////////////////////////////////////////////////////////////////////
+// C L A S S
+///////////////////////////////////////////////////////////////////////////////
+
+/**
+ * Mail Notification controller.
+ *
+ * @category   Apps
+ * @package    Mail_Notification
+ * @subpackage Controllers
+ * @author     ClearFoundation <developer@clearfoundation.com>
+ * @copyright  2011 ClearFoundation
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU General Public License version 3 or later
+ * @link       http://www.clearfoundation.com/docs/developer/apps/raid/
+ */
+
+class Mail_Notification extends ClearOS_Controller
+{
+
+    /**
+     * Mail Notification default controller
+     *
+     * @return view
+     */
+
+    function index()
+    {
+        $this->_view_edit('view');
+    }
+
+    /**
+     * Mail Notification edit controller
+     *
+     * @return view
+     */
+
+    function edit()
+    {
+        $this->_view_edit('edit');
+    }
+
+    function _view_edit($mode = null)
+    {
+        // Load dependencies
+        //------------------
+
+        $this->load->library('mail_notification/Mail_Notification');
+        $this->lang->load('mail_notification');
+
+        $data['mode'] = $mode;
+
+        // Set validation rules
+        //---------------------
+         
+        $this->form_validation->set_policy('sender', 'mail_notification/Mail_Notification', 'validate_email', TRUE);
+        $this->form_validation->set_policy('host', 'mail_notification/Mail_Notification', 'validate_host', TRUE);
+        $this->form_validation->set_policy('port', 'mail_notification/Mail_Notification', 'validate_port', TRUE);
+        $this->form_validation->set_policy('ssl', 'mail_notification/Mail_Notification', 'validate_ssl', TRUE);
+        $form_ok = $this->form_validation->run();
+
+        // Handle form submit
+        //-------------------
+
+        if (($this->input->post('submit') && $form_ok)) {
+            try {
+                $this->mail_notification->set_host($this->input->post('host'));
+                $this->mail_notification->set_port($this->input->post('port'));
+                $this->mail_notification->set_ssl($this->input->post('ssl'));
+                $this->mail_notification->set_sender($this->input->post('sender'));
+                $this->page->set_status_updated();
+                redirect('/mail_notification');
+            } catch (Exception $e) {
+                $this->page->view_exception($e);
+                return;
+            }
+        }
+
+        // Load view data
+        //---------------
+
+        try {
+            $data['host'] = $this->mail_notification->get_host();
+            $data['port'] = $this->mail_notification->get_port();
+            $data['ssl'] = $this->mail_notification->get_ssl();
+            $data['sender'] = $this->mail_notification->get_sender();
+        } catch (Exception $e) {
+            $this->page->view_exception($e);
+            return;
+        }
+
+        // Load views
+        //-----------
+
+        $this->page->view_form('settings', $data, lang('mail_notification_settings'));
+    }
+
+}
---
+++ webconfig/apps/mail_notification/trunk/language/en_US/mail_notification_lang.php
@@ -0,0 +1,15 @@
+<?php
+
+$lang['mail_notification_mail_notification'] = 'Mail Notification';
+$lang['mail_notification_host'] = 'SMTP Hostname/IP';
+$lang['mail_notification_port'] = 'SMTP Port';
+$lang['mail_notification_ssl'] = 'SSL';
+$lang['mail_notification_username'] = 'SMTP Username';
+$lang['mail_notification_password'] = 'SMTP Password';
+$lang['mail_notification_sender'] = 'Sender Address';
+$lang['mail_notification_settings'] = 'SMTP/Email Notification Settings';
+$lang['mail_notification_email_is_invalid'] = 'E-mail address is invalid.';
+$lang['mail_notification_subject_is_invalid'] = 'Subject is invalid.';
+$lang['mail_notification_host_is_invalid'] = 'SMTP hostname is invalid.';
+$lang['mail_notification_port_is_invalid'] = 'SMTP port is invalid.';
+$lang['mail_notification_ssl_is_invalid'] = 'SMTP SSL enabled is invalid.';
