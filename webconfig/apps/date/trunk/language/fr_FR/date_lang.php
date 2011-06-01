390ba45e6ef50782f9a825c388bdce19dd65e25c
pbaldwin <pbaldwin@d196f2e3-cd94-45c7-a57a-680589916e42>
2011-06-01 21:56:51 +0000

    - Updated translations
    git-svn-id: svn://svn.clearfoundation.com/clearos@1459 d196f2e3-cd94-45c7-a57a-680589916e42

--- webconfig/apps/date/trunk/language/fr_FR/date_lang.php
+++ webconfig/apps/date/trunk/language/fr_FR/date_lang.php
@@ -5,11 +5,10 @@
 $lang['date_app_summary'] = 'Time and Date';
 $lang['date_app_long_description'] = 'The time zone and clock synchronization tool.';
 $lang['date_app_tooltip'] = 'VoIP, authentication services and other applications require proper time settings, so please make sure your clock and time zone are correct!';
-$lang['date_date'] = 'La Date';
+$lang['date_date'] = 'Date';
 $lang['date_synchronize_now'] = 'Synchronize Now';
 $lang['date_time'] = 'Time';
 $lang['date_time_and_date'] = 'Time and Date';
-$lang['date_date_and_time'] = 'La Date et Lheure';
 $lang['date_time_server'] = 'Time Server';
 $lang['date_time_server_is_invalid'] = 'Time server is invalid.';
 $lang['date_time_synchronization_schedule_is_invalid'] = 'Time synchronization schedule is invalid.';
