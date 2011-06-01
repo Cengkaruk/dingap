b873abf3e2ee8da31b585fe22427277500c7e6f3
pbaldwin <pbaldwin@d196f2e3-cd94-45c7-a57a-680589916e42>
2011-06-01 19:27:07 +0000

    - Moved
    git-svn-id: svn://svn.clearfoundation.com/clearos@1456 d196f2e3-cd94-45c7-a57a-680589916e42

--- webconfig/apps/task/trunk/language/en_US/task_lang.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$lang['task_scheduler'] = 'Scheduler';
-$lang['task_task_scheduler'] = 'Task Scheduler';
-$lang['task_app_long_description'] = 'Task scheduler long description'; // FIXME
-$lang['task_app_summary'] = 'Task summary';
-$lang['task_schedule_configuration_already_exists'] = 'Schedule configuration already exists.';
