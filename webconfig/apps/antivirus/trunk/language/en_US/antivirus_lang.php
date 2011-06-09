879c8dc569fd06f1626339a517de3ded3e1c71ee
pbaldwin <pbaldwin@d196f2e3-cd94-45c7-a57a-680589916e42>
2011-06-09 22:16:00 +0000

    - Cleaned up translations tags
    git-svn-id: svn://svn.clearfoundation.com/clearos@1529 d196f2e3-cd94-45c7-a57a-680589916e42

--- webconfig/apps/antivirus/trunk/language/en_US/antivirus_lang.php
+++ webconfig/apps/antivirus/trunk/language/en_US/antivirus_lang.php
@@ -6,7 +6,6 @@ $lang['antivirus_app_tooltip'] = 'Encrypted files are often used to deliver malw
 $lang['antivirus_gateway_antivirus'] = 'Gateway Antivirus';
 $lang['antivirus_twice_a_day'] = 'Twice a Day';
 $lang['antivirus_every_two_hours'] = 'Every Two Hours';
-
 $lang['antivirus_block_encrypted_archive_flag_invalid'] = 'Block encrypted archive value is invalid.';
 $lang['antivirus_max_files_invalid'] = 'Maximum number of files is invalid.';
 $lang['antivirus_max_file_size_invalid'] = 'Maximum file size is invalid.';
@@ -16,19 +15,8 @@ $lang['antivirus_phishing_scan_urls_state'] = 'Scan URLs state is invalid.';
 $lang['antivirus_phishing_always_block_ssl_mismatch'] = 'SSL URL mismatch scan state is invalid.';
 $lang['antivirus_phishing_always_block_cloak'] = 'SSL cloaking scan state is invalid.';
 $lang['antivirus_updates_per_day_invalid'] = 'Number of updates per day is invalid.';
-
 $lang['antivirus_block_encrypted_files'] = 'Block Encrypted Files';
-$lang['antivirus_heuristics_engine'] = 'Heuristic Engine';
-$lang['antivirus_signature_engine'] = 'Signature Engine';
-$lang['antivirus_block_cloaked'] = 'Block Cloaked URLs';
 $lang['antivirus_update_interval'] = 'Update Interval';
-
-$lang['antivirus_antimalware'] = 'Antimalware';
-$lang['antivirus_antimalware_updates'] = 'Antimalware Updates';
-$lang['antivirus_last_update'] = 'Last Update';
 $lang['antivirus_maximum_files'] = 'Maximum Files in Zip Files';
 $lang['antivirus_maximum_file_size'] = 'Maximum File Size in Zip Files';
 $lang['antivirus_maximum_recursion'] = 'Maximum Recursion in Zip Files';
-$lang['antivirus_over_maximum_policy'] = 'Maximum Exceeded Policy';
-$lang['antivirus_block_ssl_mismatch'] = 'Block SSL Mismatch';
-
