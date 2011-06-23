7745bd98bd58c690ab1216ced8f594517943692a
pbaldwin <pbaldwin@d196f2e3-cd94-45c7-a57a-680589916e42>
2011-06-23 21:50:13 +0000

    - Prepped for Pootle
    git-svn-id: svn://svn.clearfoundation.com/clearos@1678 d196f2e3-cd94-45c7-a57a-680589916e42

--- webconfig/apps/antiphishing/trunk/language/en_US/antiphishing_lang.php
+++ webconfig/apps/antiphishing/trunk/language/en_US/antiphishing_lang.php
@@ -1,11 +1,9 @@
 <?php
 
-$lang['antiphishing_app_summary'] = 'Antiphishing app summary....'; // FIXME
-$lang['antiphishing_app_long_description'] = 'Antiphishing description...'; // FIXME
+$lang['antiphishing_app_description'] = 'Gateway Antivirus provides protection from your network and server.';
+$lang['antiphishing_app_name'] = 'Gateway Antiphishing';
 $lang['antiphishing_app_tooltip'] = 'Even the most savvy Internet users can accidentally click on a phishing link.  We recommend leaving all antiphishing features enabled.';
-$lang['antiphishing_gateway_antiphishing'] = 'Gateway Antiphishing';
-
-$lang['antiphishing_heuristics_engine'] = 'Heuristic Engine';
-$lang['antiphishing_signature_engine'] = 'Signature Engine';
 $lang['antiphishing_block_cloaked'] = 'Block Cloaked URLs';
 $lang['antiphishing_block_ssl_mismatch'] = 'Block SSL Mismatch';
+$lang['antiphishing_heuristics_engine'] = 'Heuristic Engine';
+$lang['antiphishing_signature_engine'] = 'Signature Engine';
