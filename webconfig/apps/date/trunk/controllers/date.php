d14b9af27b78205cc2cde11d3207e9030c272607
pbaldwin <pbaldwin@d196f2e3-cd94-45c7-a57a-680589916e42>
2011-06-24 01:20:09 +0000

    - Updated translations
    git-svn-id: svn://svn.clearfoundation.com/clearos@1680 d196f2e3-cd94-45c7-a57a-680589916e42

--- webconfig/apps/date/trunk/controllers/date.php
+++ webconfig/apps/date/trunk/controllers/date.php
@@ -98,7 +98,7 @@ class Date extends ClearOS_Controller
         // Load views
         //-----------
 
-        $this->page->view_form('date', $data, lang('date_date'));
+        $this->page->view_form('date', $data, lang('date_app_name'));
     }
 
     /**
--- webconfig/apps/date/trunk/deploy/info.php
+++ webconfig/apps/date/trunk/deploy/info.php
@@ -11,14 +11,14 @@ $app['vendor'] = 'ClearFoundation';
 $app['packager'] = 'ClearFoundation';
 $app['license'] = 'GPLv3';
 $app['license_core'] = 'LGPLv3';
-$app['summary'] = lang('date_app_summary');
-$app['description'] = lang('date_app_long_description');
+$app['summary'] = lang('date_app_name');
+$app['description'] = lang('date_app_description');
 
 /////////////////////////////////////////////////////////////////////////////
 // App name and categories
 /////////////////////////////////////////////////////////////////////////////
 
-$app['name'] = lang('date_date_and_time');
+$app['name'] = lang('date_app_name');
 $app['category'] = lang('base_category_system');
 $app['subcategory'] = lang('base_subcategory_settings');
 
