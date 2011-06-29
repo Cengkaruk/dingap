65745b8971fccceea1bc0ade06315fb3ef68a36e
abylund <abylund@d196f2e3-cd94-45c7-a57a-680589916e42>
2011-06-29 23:37:06 +0000

    - Synced languages
    git-svn-id: svn://svn.clearfoundation.com/clearos@1719 d196f2e3-cd94-45c7-a57a-680589916e42

--- webconfig/apps/language/trunk/language/fr_FR/language_lang.php
+++ webconfig/apps/language/trunk/language/fr_FR/language_lang.php
@@ -4,7 +4,9 @@
 
 $lang['language_app_description'] = 'This software is provided in many different languages.  Use this tool to set the default language for the system.';
 $lang['language_app_name'] = 'Language';
+$lang['language_default_system_language'] = 'Default System Language';
 $lang['language_keyboard'] = 'Keyboard';
 $lang['language_keyboard_is_invalid'] = 'Keyboard is invalid.';
-$lang['language_language'] = 'La Langue';
+$lang['language_language'] = 'Language';
 $lang['language_language_is_invalid'] = 'Language is invalid.';
+$lang['language_update_my_login_session'] = 'Update My Login Session';
