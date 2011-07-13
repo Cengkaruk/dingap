a9eb60cee16c092c824dd80c9764278add0b75e7
pbaldwin <pbaldwin@d196f2e3-cd94-45c7-a57a-680589916e42>
2011-07-13 20:36:19 +0000

    - Added bandwidth
    git-svn-id: svn://svn.clearfoundation.com/clearos@1828 d196f2e3-cd94-45c7-a57a-680589916e42

---
+++ webconfig/apps/bandwidth/trunk/deploy/info.php
@@ -0,0 +1,46 @@
+<?php
+
+/////////////////////////////////////////////////////////////////////////////
+// General information
+/////////////////////////////////////////////////////////////////////////////
+
+$app['basename'] = 'bandwidth';
+$app['version'] = '5.9.9.2';
+$app['release'] = '4';
+$app['vendor'] = 'ClearFoundation';
+$app['packager'] = 'ClearFoundation';
+$app['license'] = 'GPLv3';
+$app['license_core'] = 'LGPLv3';
+$app['description'] = lang('bandwidth_app_description');
+
+/////////////////////////////////////////////////////////////////////////////
+// App name and categories
+/////////////////////////////////////////////////////////////////////////////
+
+$app['name'] = lang('bandwidth_app_name');
+$app['category'] = lang('base_category_network');
+$app['subcategory'] = lang('base_subcategory_bandwidth_and_qos');
+
+/////////////////////////////////////////////////////////////////////////////
+// Controllers
+/////////////////////////////////////////////////////////////////////////////
+
+$app['controllers']['bandwidth']['title'] = lang('bandwidth_app_name');
+
+/////////////////////////////////////////////////////////////////////////////
+// Packaging
+/////////////////////////////////////////////////////////////////////////////
+
+$app['requires'] = array(
+    'app-network',
+);
+
+$app['core_requires'] = array(
+    'app-network-core',
+    'app-firewall-core',
+);
+
+$app['core_directory_manifest'] = array(
+    '/var/clearos/bandwidth' => array(),
+    '/var/clearos/bandwidth/backup/' => array(),
+);
---
+++ webconfig/apps/bandwidth/trunk/language/en_US/bandwidth_lang.php
@@ -0,0 +1,4 @@
+<?php
+
+$lang['bandwidth_app_description'] = 'Bandwidth Manager description...'; // FIXME
+$lang['bandwidth_app_name'] = 'Bandwidth Manager';
