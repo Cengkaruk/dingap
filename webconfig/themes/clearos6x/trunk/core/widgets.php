69823d975108e002c48dd2682bc068d3d2c9a77c
bchambers <bchambers@d196f2e3-cd94-45c7-a57a-680589916e42>
2011-08-26 21:07:38 +0000

    - encode special characters / and <,> to reduce browser log noise
    git-svn-id: svn://svn.clearfoundation.com/clearos@2228 d196f2e3-cd94-45c7-a57a-680589916e42

--- webconfig/themes/clearos6x/trunk/core/widgets.php
+++ webconfig/themes/clearos6x/trunk/core/widgets.php
@@ -1268,7 +1268,7 @@ function theme_summary_box($data)
                                 $('tbody', $('#sidebar_summary_table')).append(
                                     c_row(
                                         '" . lang('base_status') . "',
-                                        '<span style=\'color: red\'>" . lang('marketplace_redemption') . "</span>'
+                                        '&lt;span style=\'color: red\'&gt;" . lang('marketplace_redemption') . "&lt;\\/span&gt;'
                                     )
                                 );
                             }
@@ -1277,7 +1277,7 @@ function theme_summary_box($data)
                                 $('tbody', $('#sidebar_summary_table')).append(
                                     c_row(
                                         '" . lang('base_status') . "',
-                                        '<span style=\'color: red\'>" . lang('marketplace_expired_no_subsription') . "</span>'
+                                        '&lt;span style=\'color: red\'&gt;" . lang('marketplace_expired_no_subsription') . "&lt;\\/span&gt;'
                                     )
                                 );
                             }
@@ -1290,7 +1290,7 @@ function theme_summary_box($data)
             }
 
             function c_row(field, value) {
-                return '<tr><td><b>' + field + '</b></td><td>' + value + '</td></tr>';
+                return '<tr><td><b>' + field + '<\\/b><\\/td><td>' + value + '<\\/td><\\/tr>';
             }
 
         </script>";
