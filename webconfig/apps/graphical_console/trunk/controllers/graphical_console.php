1a7bd13b90e16fa8bc5a96deff6013285ed1b5bb
pbaldwin <pbaldwin@d196f2e3-cd94-45c7-a57a-680589916e42>
2011-11-08 00:45:20 +0000

    - Changed method name in network class
    git-svn-id: svn://svn.clearfoundation.com/clearos@2486 d196f2e3-cd94-45c7-a57a-680589916e42

--- webconfig/apps/graphical_console/trunk/controllers/graphical_console.php
+++ webconfig/apps/graphical_console/trunk/controllers/graphical_console.php
@@ -72,7 +72,7 @@ class Graphical_Console extends ClearOS_Controller
         //------------------
 
         try {
-            $data['lan_ips'] = $this->iface_manager->get_lan_ips();
+            $data['lan_ips'] = $this->iface_manager->get_trusted_ips();
             $data['os_name'] = $this->os->get_name();
             $data['os_version'] = $this->os->get_version();
         } catch (Engine_Exception $e) {
