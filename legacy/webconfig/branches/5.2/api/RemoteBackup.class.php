420310edc362fccce06483aa8de24f3cca16ddfd
dsokoloski <dsokoloski@d196f2e3-cd94-45c7-a57a-680589916e42>
2011-11-10 15:33:01 +0000

    - Added methods to get/set rsync bandwidth limit.
    git-svn-id: svn://svn.clearfoundation.com/clearos@2514 d196f2e3-cd94-45c7-a57a-680589916e42

--- legacy/webconfig/branches/5.2/api/RemoteBackup.class.php
+++ legacy/webconfig/branches/5.2/api/RemoteBackup.class.php
@@ -365,6 +365,20 @@ class RemoteBackup extends Engine
 		}
 	}
 
+	final public function SetBandwidthLimit($kbps)
+	{
+		$file = new File(RemoteBackupService::FILE_CONF, true);
+		try {
+			if (!$file->Exists())
+				$file->Create('root', 'root', '0600');
+			$this->ReplaceValue($file,
+				'/^[[:space:]]*rsync-bwlimit[[:space:]]*=.*$/',
+				sprintf("rsync-bwlimit = %d\n", $kbps));
+		} catch (Exception $e) {
+			throw new EngineException($e->getMessage(), COMMON_ERROR);
+		}
+	}
+
 	final public function ResetKey($key)
 	{
 		$this->SetKey($key);
@@ -379,6 +393,19 @@ class RemoteBackup extends Engine
 		$file = new File(RemoteBackupService::FILE_CONF, true);
 		try {
 			return $file->LookupValue('/^[[:space:]]*key[[:space:]]*=[[:space:]]*/');
+		} catch (FileNoMatchException $e) {
+			return null;
+		}
+		catch (Exception $e) {
+			throw new EngineException($e->getMessage(), COMMON_ERROR);
+		}
+	}
+
+	final public function GetBandwidthLimit()
+	{
+		$file = new File(RemoteBackupService::FILE_CONF, true);
+		try {
+			return $file->LookupValue('/^[[:space:]]*rsync-bwlimit[[:space:]]*=[[:space:]]*/');
 		} catch (FileNoMatchException $e) {
 			return null;
 		}
