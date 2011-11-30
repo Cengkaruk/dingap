3e7a428e5354925e7462e3c876b12f0fbdb89b41
pbaldwin <pbaldwin@d196f2e3-cd94-45c7-a57a-680589916e42>
2011-11-30 02:08:20 +0000

    - Removed invalid tag
    git-svn-id: svn://svn.clearfoundation.com/clearos@2774 d196f2e3-cd94-45c7-a57a-680589916e42

--- webconfig/apps/dns/tags/6.1.0.beta2-1/language/en_US/dns_lang.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$lang['dns_alias'] = 'Alias';
-$lang['dns_app_description'] = 'The local DNS server can be used for mapping IP addresses on your network to hostnames.';
-$lang['dns_app_name'] = 'DNS Server';
-$lang['dns_dns_entry'] = 'DNS Entry';
-$lang['dns_dns_server'] = 'DNS Server';
