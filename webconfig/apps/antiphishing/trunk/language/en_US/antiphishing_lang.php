f05a584485b17b92a7eaa4878b2f719221a0f119
pbaldwin <pbaldwin@d196f2e3-cd94-45c7-a57a-680589916e42>
2011-11-30 01:29:48 +0000

    - Fixed translation
    git-svn-id: svn://svn.clearfoundation.com/clearos@2769 d196f2e3-cd94-45c7-a57a-680589916e42

--- webconfig/apps/antiphishing/trunk/language/en_US/antiphishing_lang.php
+++ webconfig/apps/antiphishing/trunk/language/en_US/antiphishing_lang.php
@@ -1,7 +1,7 @@
 <?php
 
 # -*- coding: utf-8 -*-
-$lang['antiphishing_app_description'] = 'Gateway Antivirus provides protection from your network and server.';
+$lang['antiphishing_app_description'] = 'Gateway Antiphishing provides protection from your network and server.';
 $lang['antiphishing_app_name'] = 'Gateway Antiphishing';
 $lang['antiphishing_app_tooltip'] = 'Even the most savvy Internet users can accidentally click on a phishing link.  We recommend leaving all antiphishing features enabled.';
 $lang['antiphishing_block_cloaked'] = 'Block Cloaked URLs';
