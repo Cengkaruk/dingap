bfc5d0c11cfe5370d6a37673709a04907c802b98
pbaldwin <pbaldwin@d196f2e3-cd94-45c7-a57a-680589916e42>
2011-12-18 10:46:24 +0000

    - Added sysconfig changes
    git-svn-id: svn://svn.clearfoundation.com/clearos@2846 d196f2e3-cd94-45c7-a57a-680589916e42

--- webconfig/apps/openldap/trunk/deploy/info.php
+++ webconfig/apps/openldap/trunk/deploy/info.php
@@ -5,7 +5,7 @@
 /////////////////////////////////////////////////////////////////////////////
 
 $app['basename'] = 'openldap';
-$app['version'] = '6.1.0.beta2';
+$app['version'] = '6.1.0.beta2.1';
 $app['release'] = '1';
 $app['vendor'] = 'ClearFoundation';
 $app['packager'] = 'ClearFoundation';
