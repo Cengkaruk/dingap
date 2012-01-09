55b7a209d6a3ec7c0663dc7bc8be87a7d0e96c6d
pbaldwin <pbaldwin@d196f2e3-cd94-45c7-a57a-680589916e42>
2012-01-09 23:52:08 +0000

    - Prepped for beta3 release
    git-svn-id: svn://svn.clearfoundation.com/clearos@2916 d196f2e3-cd94-45c7-a57a-680589916e42

--- webconfig/apps/print_server/trunk/packaging/print_server.php
+++ webconfig/apps/print_server/trunk/packaging/print_server.php
@@ -17,6 +17,6 @@ clearos_load_language('print_server_plugin');
 // C O N F I G
 ///////////////////////////////////////////////////////////////////////////////
 
-$plugin['name'] = lang('print_server_plugin_print_server');
+$plugin['name'] = lang('print_server_app_name');
 $plugin['nickname'] = lang('print_server_plugin_print_server');
-$plugin['description'] = lang('print_server_plugin_description');
+$plugin['description'] = lang('print_server_app_description');
